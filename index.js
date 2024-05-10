514259e39ea8ea7432b0932cd68aa70bcb26e6de
Shubham <117731348+shubham22062@users.noreply.github.com>
2024-05-10 15:36:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    let expression = document.getElementById('display').value;
+    let result = eval(expression);
+    document.getElementById('display').value = result;
+}
